47c99c8ce105b28bd8ac2b27c97be419d1c31c01
Soyeb <tssoyeb@gmail.com>
2022-02-09 22:32:17 +0600

    first commit

---
+++ event/script.js
@@ -0,0 +1,82 @@
+
+
+// //handle bluebutton click
+// const bluebutton=document.getElementById('make-bluebutton');
+
+// bluebutton.onclick=makeblue;
+
+
+
+// function makeblue(){
+//     document.body.style.backgroundColor='blue';
+
+// }
+// //handle greenbutton using annonymous function
+// const greenbutton =document.getElementById('make-greenbutton');
+//       greenbutton.onclick = function (){
+//           document.body.style.backgroundColor='green';
+//       }
+
+//       //handle blackbuton using addEventListener
+//       const blackbutton=document.getElementById('make-blackbutton');
+//       blackbutton.addEventListener('click', makeblack)
+
+//       function makeblack(){
+//           document.body.style.backgroundColor='black';
+//       }
+
+//     //addEventListener
+
+//     const orangebutton= document.getElementById('make-orangebutton');
+//     orangebutton.addEventListener('click',function(){
+
+//         document.body.style.backgroundColor='orange';
+//     })
+
+//     //addEventListener using shortcut
+
+//     document.getElementById('make-pinkbutton').addEventListener('click',function(){
+//     document.body.style.backgroundColor='hotpink';
+
+//     })
+
+
+//     //js for event-common.html ----------------------------------------------------------
+//     //using onclick function
+//   function clickingmethod(){
+//         const p =document.getElementById('clicking-method');
+//         p.innerText='using onclick function';
+//     }
+
+//     //direct addEventListener
+   
+        
+
+//         //update by input box
+
+
+
+
+
+//         //comment.html----------------------------
+
+//            //add button event handler
+//            document.getElementById('submit-button').
+//            addEventListener('click',function(){
+    
+//             //get textArea
+//             const commentBox= document.getElementById('new-comment');
+    
+//             // create paragraph comment
+//             const newComment = document.createElement('p');
+//             newComment.innerText=commentBox.value;
+    
+//             //appendchild to the parent 
+//             const commentContainer = document.getElementById('container');
+//             commentContainer.appendChild(newComment);
+//             //clear textArea after post
+//             commentBox.value='';
+    
+//            })
+const x =document.getElementById('pear');
+console.log(x);
